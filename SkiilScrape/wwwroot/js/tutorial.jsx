154690ddc2e63af7a9c79b20c90f06d6872f346a
hassan635 <hassan.a.tariq@gmail.com>
2020-08-10 15:16:46 +1000

    Updated comments

--- SkiilScrape/wwwroot/js/tutorial.jsx
+++ SkiilScrape/wwwroot/js/tutorial.jsx
@@ -47,7 +47,7 @@ class CommentForm extends React.Component {
         return (
             <form className="commentForm">
                 <input type="text" placeholder="Your name" />
-                <input type="text" placeholder="Say something..." />
+                <input type="text" placeholder="Say..." />
                 <input type="submit" value="Post" />
             </form>
         );
