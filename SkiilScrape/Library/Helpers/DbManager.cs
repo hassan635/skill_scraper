8c37da0a8035b7d36b584ef42867c56e5c82b93b
hassan635 <hassan.a.tariq@gmail.com>
2020-08-11 22:21:25 +1000

    MongoDB setup finished

---
+++ SkiilScrape/Library/Helpers/DbManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MongoDB.Driver;
+namespace SkiilScrape.Library.Helpers
+{
+    public class DbManager
+    {
+        private MongoClient mongoClient = null;
+         
+        public DbManager()
+        {
+            var dbclient = new MongoClient("mongodb+srv://tezt:Windows.2000@cluster0-gpynn.mongodb.net/<dbname>?retryWrites=true&w=majority");
+            var db = dbclient.GetDatabase("testdb");
+        }
+    }
+}
